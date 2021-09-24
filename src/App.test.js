468dc2c2549247f9aff73e5426327f251e45ed3d
Rahul <coolrahul1729@gmail.com>
2021-09-24 19:35:08 +0530

    corrected tests

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hi there/);
+  const linkElement = screen.getByText(/Docker travis ci elasticbeanstalk/);
   expect(linkElement).toBeInTheDocument();
 });
